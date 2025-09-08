b46cd1a7d08f5ec33b879ebae229dafff6de5ab7
Aisaac <129123213+Aisaac123@users.noreply.github.com>
2025-09-08 18:23:11 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     content: [
-        "./resources/views/components/sections/heading-title.blade.php",
         "./resources/**/*.blade.php",
         "./app/Filament/**/*.php",
         "./resources/views/filament/**/*.blade.php",
