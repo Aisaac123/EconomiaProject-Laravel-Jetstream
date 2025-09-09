ccb5dbf30f134fe836cd37b8d8f5b3fd128d855e
brisaac08 <bicaro@unicesar.edu.co>
2025-09-09 01:54:53 -0500

    add interes simple: calculo y vista preliminar

--- resources/views/filament/pages/interes-simple.blade.php
+++ resources/views/filament/pages/interes-simple.blade.php
@@ -1,3 +1,16 @@
-<x-filament-panels::page>
-    {{-- Page content --}}
-</x-filament-panels::page>
+{{-- filepath: resources/views/filament/pages/interes-simple.blade.php --}}
+<x-filament::page>
+    <div class="space-y-4">
+        {{-- Formulario generado desde el Schema --}}
+        {{ $this->form }}
+
+        {{-- Resultado si existe --}}
+        @if($data['resultado'] ?? false)
+            <x-filament::card>
+                <div class="text-lg font-bold">
+                    Resultado (Interés Simple): ${{ number_format($data['resultado'], 2) }}
+                </div>
+            </x-filament::card>
+        @endif
+    </div>
+</x-filament::page>
