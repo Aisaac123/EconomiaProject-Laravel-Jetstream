e3faa33eac9608c28705d4063bab1db8d6ebf1b7
brisaac08 <bicaro@unicesar.edu.co>
2025-09-14 21:29:24 -0500

    add modelo interactivo tasa de interes

---
+++ app/Filament/Widgets/InteresInteractivoChart.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class InteresInteractivoChart extends Widget
+{
+    protected string $view = 'filament.widgets.interes-interactivo-chart';
+}
+
